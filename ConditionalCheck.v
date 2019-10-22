3097e308844e9eaa621d3b54861796793674cddc
Mohammad Hosein Azizian <mohammadh.azizian@gmail.com>
2019-10-22 10:27:41 +0330

    refactor conditional check

--- ConditionalCheck.v
+++ ConditionalCheck.v
@@ -14,64 +14,65 @@ module ConditionalCheck (
     always @(cond) begin
 
         case(cond)
-        `COND_EQ : begin
-            condState <= z;
-        end
+            `COND_EQ : begin
+                condState <= z;
+            end
 
-        `COND_NE : begin
-            condState <= ~z;
-        end
+            `COND_NE : begin
+                condState <= ~z;
+            end
 
-        `COND_CS_HS : begin
-            condState <= c;
-        end
+            `COND_CS_HS : begin
+                condState <= c;
+            end
 
-        `COND_CC_LO : begin
-            condState <= ~c;
-        end
+            `COND_CC_LO : begin
+                condState <= ~c;
+            end
 
-        `COND_MI : begin
-            condState <= n;
-        end
+            `COND_MI : begin
+                condState <= n;
+            end
 
-        `COND_PL : begin
-            condState <= ~n;
-        end
+            `COND_PL : begin
+                condState <= ~n;
+            end
 
-        `COND_VS : begin
-            condState <= v;
-        end
+            `COND_VS : begin
+                condState <= v;
+            end
 
-        `COND_VC : begin
-            condState <= ~v;
-        end
+            `COND_VC : begin
+                condState <= ~v;
+            end
 
-        `COND_HI : begin
-            condState <= c & ~z;
-        end
+            `COND_HI : begin
+                condState <= c & ~z;
+            end
 
-        `COND_LS : begin
-            condState <= ~c & z;
-        end
+            `COND_LS : begin
+                condState <= ~c & z;
+            end
 
-        `COND_GE : begin
-            condState <= (n & v) | (~n & ~v);
-        end
+            `COND_GE : begin
+                condState <= (n & v) | (~n & ~v);
+            end
 
-        `COND_LT : begin
-            condState <= (n & ~v) | (~n & v);
-        end
+            `COND_LT : begin
+                condState <= (n & ~v) | (~n & v);
+            end
 
-        `COND_GT : begin
-            condState <= ~z & ((n & v) | (~n & ~v));
-        end
+            `COND_GT : begin
+                condState <= ~z & ((n & v) | (~n & ~v));
+            end
 
-        `COND_LE : begin
-            condState <= z & ((n & ~v) | (~n & v));
-        end
+            `COND_LE : begin
+                condState <= z & ((n & ~v) | (~n & v));
+            end
 
-        `COND_LE : begin
-            condState <= 1`b1;
-        end
+            `COND_LE : begin
+                condState <= 1`b1;
+            end
+        endcase
     end
 endmodule
