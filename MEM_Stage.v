e2cf1c78663de4513cc97eebfe7e2a5d772c4ac5
Mohammad Hosein Azizian <mohammadh.azizian@gmail.com>
2019-11-12 10:26:22 +0330

    Add Mem stage

--- MEM_Stage.v
+++ MEM_Stage.v
@@ -1,8 +1,23 @@
+`include "Defines.v"
+
 module MEM_Stage(
-	input clk,
-	input rst,
-	input[31:0] PC_in,
-	output[31:0] PC
+	input clk, rst,
+	input wb_en_in, mem_r_en_in, mem_w_en_in,
+	input [`REGISTER_LEN - 1:0] alu_res_in, val_Rm,
+	input [`REG_ADDRESS_LEN - 1:0] dest_in,
+
+	output wb_en_out, mem_r_en_out,
+	output [`REGISTER_LEN - 1:0] mem_out, alu_res_out,
+	output [`REG_ADDRESS_LEN - 1:0] dest_out
 );
-	assign PC = PC_in;
+	assign wb_en_out = wb_en_in;
+	assign mem_r_en_out = mem_r_en_in;
+	assign alu_res_out = alu_res_in;
+	assign dest_out = dest_in;
+
+	Memory memory(.clk(clk), .rst(rst), .address(alu_res_in),
+			.WriteData(val_Rm), .MemRead(mem_r_en_in), .MemWrite(mem_w_en_in),
+			.ReadData(mem_out)
+	);
+
 endmodule
