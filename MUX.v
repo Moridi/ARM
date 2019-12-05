166c32b4d749e49f89538ac88624cf68f8d44d18
moridi <m.moridi.2009@gmail.com>
2019-12-05 14:41:08 +0330

    Add multiplexers for alu sources

--- MUX.v
+++ MUX.v
@@ -1,3 +1,5 @@
+`include "Defines.v"
+
 module MUX_2_to_1(first, second, sel_first, sel_second, out);
   
 	parameter WORD_LENGTH = 32;
@@ -6,5 +8,18 @@ module MUX_2_to_1(first, second, sel_first, sel_second, out);
 	output[WORD_LENGTH - 1:0] out;
 	
 	assign out = sel_first ? first : (sel_second ? second : out); 
+	
+endmodule
+
+module MUX_3_to_1(first, second, third, sel, out);
+  
+	parameter WORD_LENGTH = 32;
+	input [WORD_LENGTH - 1:0] first, second, third;
+	input [1:0] sel;
+	output [WORD_LENGTH - 1:0] out;
+	
+	assign out = (sel == `FORW_SEL_FROM_ID) ? first :
+			((sel == `FORW_SEL_FROM_WB) ? second :
+			((sel == `FORW_SEL_FROM_MEM) ? third : out)); 
 	
 endmodule
