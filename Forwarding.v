89608579a17cf82df2825027d810b627918bafeb
Mohammad Hosein Azizian <mohammadh.azizian@gmail.com>
2019-12-05 17:34:42 +0330

    Amend forwarding moduel

--- Forwarding.v
+++ Forwarding.v
@@ -1,8 +1,8 @@
 `include "Defines.v"
 
 module Formarding (
-	input RB_wb_en, MEM_wb_en,
-    input [3:0] MEM_dst, RB_dst, ID_src1, ID_src2,
+	input WB_wb_en, MEM_wb_en,
+    input [3:0] MEM_dst, WB_dst, ID_src1, ID_src2,
 	
     output [1:0] sel_src1, sel_src2,
     output ignore_hazard
@@ -21,6 +21,18 @@ module Formarding (
 		sel_src2_temp = 2'b0;
         ignore_hazard_temp = 1'b0;
         
+        if (WB_wb_en) begin
+            if (WB_dst == ID_src1) begin
+                sel_src1_temp = `FORW_SEL_FROM_WB;
+                ignore_hazard_temp = 1'b1;
+            end
+            
+            if (WB_dst == ID_src2) begin
+                sel_src2_temp = `FORW_SEL_FROM_WB;
+                ignore_hazard_temp = 1'b1;
+            end
+        end
+        
         if (MEM_wb_en) begin
             if (MEM_dst == ID_src1) begin
                 sel_src1_temp = `FORW_SEL_FROM_MEM;
@@ -29,17 +41,6 @@ module Formarding (
             
             if (MEM_dst == ID_src2) begin
                 sel_src2_temp = `FORW_SEL_FROM_MEM;
-                ignore_hazard_temp = 1'b1;
-            end
-        end
-        if (RB_wb_en) begin
-            if (RB_dst == ID_src1) begin
-                sel_src1_temp = `FORW_SEL_FROM_RB;
-                ignore_hazard_temp = 1'b1;
-            end
-            
-            if (RB_dst == ID_src2) begin
-                sel_src2_temp = `FORW_SEL_FROM_RB;
                 ignore_hazard_temp = 1'b1;
             end
         end
